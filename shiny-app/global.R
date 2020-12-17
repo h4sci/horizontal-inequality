702ba90122ca168f128bcaeb7ba243b148143a5d
dario <dario.meili@nadel.ethz.ch>
2020-12-17 15:27:04 +0100

    filtered only last DHS round, to be expanded later

--- shiny-app/global.R
+++ shiny-app/global.R
@@ -15,7 +15,9 @@ world <- ne_countries(scale = "medium", returnclass = "sf") %>%
 master_data <- read_csv("raw-data/master_data.csv")
 
 # make long format 
-ineq <- master_data %>% 
+data <- master_data %>% 
   select(country, year, "grouping_var" = category, measure, gini_sch, gini_chm, gini_chm, gini_wlth) %>% 
-  pivot_longer(cols = starts_with("gini_"), names_to = "outcome_var", values_to = "gini_value")
+  pivot_longer(cols = starts_with("gini_"), names_to = "outcome_var", values_to = "gini_value") %>% 
+  filter(year>=2013)
+ 
 

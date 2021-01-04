ab30bfde56182b1dba4afb5c56c0655ffeb04712
dario <dario.meili@nadel.ethz.ch>
2021-01-04 12:22:59 +0100

    long data format for master DF with ISO identifiers

--- shiny-app/global.R
+++ shiny-app/global.R
@@ -12,12 +12,11 @@ world <- ne_countries(scale = "medium", returnclass = "sf") %>%
   select(name_long, brk_name, iso_a3)
 
 # read inequality data
-master_data <- read_csv("raw-data/master_data.csv")
+master_data <- read_csv("raw-data/master_data_ISO_phase.csv")
 
 # make long format 
 data <- master_data %>% 
-  select(country, year, "grouping_var" = category, measure, gini_sch, gini_chm, gini_chm, gini_wlth) %>% 
-  pivot_longer(cols = starts_with("gini_"), names_to = "outcome_var", values_to = "gini_value") %>% 
-  filter(year>=2013)
+  select(country, year, ISO, phase, "grouping_var" = category, measure, gini_sch, gini_chm, gini_st, gini_wlth, ) %>% 
+  pivot_longer(cols = starts_with("gini_"), names_to = "outcome_var", values_to = "gini_value")
  
 

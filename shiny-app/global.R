53be76ee02a7f0d6ec089f0c12e6f481977bfa44
dario <dario.meili@nadel.ethz.ch>
2021-01-04 12:29:16 +0100

    only include DHS rounds after 2013 for simplicity (can be changed later)

--- shiny-app/global.R
+++ shiny-app/global.R
@@ -17,6 +17,7 @@ master_data <- read_csv("raw-data/master_data_ISO_phase.csv")
 # make long format 
 data <- master_data %>% 
   select(country, year, ISO, phase, "grouping_var" = category, measure, gini_sch, gini_chm, gini_st, gini_wlth, ) %>% 
-  pivot_longer(cols = starts_with("gini_"), names_to = "outcome_var", values_to = "gini_value")
+  pivot_longer(cols = starts_with("gini_"), names_to = "outcome_var", values_to = "gini_value") %>% 
+  filter(year >= 2013)
  
 
